5270ca84134dc9730f5b074da663d32fc9fbad9f
marylisho <marylishop@yahoo.com>
2023-07-12 22:25:49 +0400

    adding tasks

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
 def fizzbuzz():
-	for i in range(1, 101):
-		output = ""
-        if i % 3 == 0:
-            output += "Fizz"
-	if i % 5 == 0:
-		output += "Buzz"
-			print(output or i, end=" ")
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz ", end="")
+        elif i % 5 == 0:
+            print("Buzz ", end="")
+        elif i % 3 == 0:
+            print("Fizz ", end="")
+        else:
+            print("{:d} ".format(i), end="")
