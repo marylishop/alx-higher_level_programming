3ed42086033131ea7ee6c52220f867f86d4312a3
marylisho <marylishop@yahoo.com>
2023-07-12 22:40:57 +0400

    adding task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python3
 def islower(c):
-	ascii_val = ord(c)
-	if ascii_val >= 97 and ascii_val <= 122:
-	return True
-	return False
+    if ord(c) > 96:
+        return True
+    return False
